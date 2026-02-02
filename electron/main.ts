55276ec7a455945cabf8189a7c2171c33383938d
윤효진 <hyojinee08@naver.com>
2026-02-02 20:51:31 +0900

    chore: electron-is-dev

--- electron/main.ts
+++ electron/main.ts
@@ -1,13 +1,14 @@
-import { app, Tray, BrowserWindow, nativeImage } from "electron";
+import { app, Tray, BrowserWindow, nativeImage, screen } from "electron";
 import path from "path";
+import isDev from "electron-is-dev";
 
 let tray: Tray | null = null;
 let window: BrowserWindow | null = null;
 
 function createTray() {
-    const iconPath = app.isPackaged
-        ? path.join(process.resourcesPath, "icon.png")
-        : path.join(process.cwd(), "public", "icon.png");
+    const iconPath = isDev
+        ? path.join(process.cwd(), "public", "tray.png")
+        : path.join(process.resourcesPath, "tray.png");
 
     const icon = nativeImage.createFromPath(iconPath);
     icon.setTemplateImage(true);
@@ -18,12 +19,11 @@ function createTray() {
     tray.on("click", (_, bounds) => {
         if (!window || !bounds) return;
 
-        const { x, y, width, height } = bounds;
-        const { width: w } = window.getBounds();
+        const { x, y } = bounds;
 
         window.setPosition(
-            Math.round(x + width / 2 - w / 2),
-            Math.round(y + height + 5),
+            Math.round(x), 
+            Math.round(y),
             false,
         );
 
@@ -47,9 +47,11 @@ function createWindow() {
         title: "simple timer",
     });
 
-    app.isPackaged
-        ? window.loadFile(path.join(process.resourcesPath, "renderer", "index.html"))
-        : window.loadURL("http://localhost:5173");
+    window.loadURL(
+        isDev
+            ? "http://localhost:5173"
+            : `file://${ path.join(process.resourcesPath, "renderer", "index.html") }`,
+    );
 
     window.on("blur", () => window?.hide());
 }
