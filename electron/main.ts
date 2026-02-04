2984031d0b11829bcfb8caa4d928f85ba0833861
윤효진 <hyojinee08@naver.com>
2026-02-04 14:30:47 +0900

    refactor: main.ts

--- electron/main.ts
+++ electron/main.ts
@@ -1,48 +1,12 @@
-import { app, BrowserWindow, nativeImage, Tray } from "electron";
+import { app, Tray, BrowserWindow, nativeImage } from "electron";
+import * as path from "path";
 import isDev from "electron-is-dev";
-import path from "path";
 
 let tray: Tray | null = null;
 let window: BrowserWindow | null = null;
 
-const getWindowPosition = () => {
-    if (!window || !tray) return { x: 0, y: 0 };
-
-    const windowBounds = window.getBounds();
-    const trayBounds = tray.getBounds();
-
-    const x = Math.round(
-        trayBounds.x + trayBounds.width / 2 - windowBounds.width / 2
-    );
-
-    const y = Math.round(
-        trayBounds.y + trayBounds.height + 4
-    );
-
-    return { x, y };
-}
-
-const toggleWindow = () => {
-    if (!window) return;
-
-    const { x, y } = getWindowPosition();
-
-    window.setBounds({
-        x,
-        y,
-        width: window.getBounds().width,
-        height: window.getBounds().height
-    });
-
-    window.isVisible()
-        ? window.hide()
-        : (window.show(), window.focus());
-}
-
 const createTray = () => {
-    const iconPath = isDev
-        ? path.join(process.cwd(), "public", "tray.png")
-        : path.join(process.resourcesPath, "assets", "tray.png");
+    const iconPath = path.join(app.getAppPath(), "public", "tray.png");
 
     const icon = nativeImage.createFromPath(iconPath);
     icon.setTemplateImage(true);
@@ -51,34 +15,57 @@ const createTray = () => {
     tray.setToolTip("Simple Timer");
 
     tray.on("click", () => toggleWindow());
+};
+
+const setWindowPosition = () => {
+    if (!tray || !window) return;
+
+    const trayBounds = tray.getBounds();
+    const windowBounds = window.getBounds();
+
+    const x = Math.round(
+        trayBounds.x + trayBounds.width / 2 - windowBounds.width / 2
+    );
+
+    const y = Math.round(trayBounds.y + trayBounds.height + 4);
+
+    window.setPosition(x, y, false);
+}
+
+const toggleWindow = () => {
+    if (!window) return;
+
+    window.isVisible()
+        ? window.hide()
+        : (window.show(), setWindowPosition());
 }
 
 const createWindow = () => {
     window = new BrowserWindow({
         width: 300,
         height: 450,
-        show: false,
         frame: false,
         resizable: false,
+        show: false,
+        alwaysOnTop: true,
+        fullscreenable: false,
         webPreferences: {
+            /** */
+        },
+    });
 
-        }
+    window.setVisibleOnAllWorkspaces(true, {
+        visibleOnFullScreen: true,
     });
 
     window.loadURL(
         isDev
             ? "http://localhost:5173"
-            : `file://${path.join(__dirname, "../build/index.html")}`
+            : `file://${path.join(app.getAppPath(), "dist", "index.html")}`
     );
+};
 
-    window.on("blur", () => window?.hide());
-}
-
-app.on("ready", () => {
-    if (process.platform === "darwin") {
-        app.dock?.hide();
-    }
-
+app.whenReady().then(() => {
     createTray();
     createWindow();
 });
